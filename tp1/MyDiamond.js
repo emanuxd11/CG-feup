bafb17e48f431891cb69c56b69e912bdc14df73b
Emanuel Maia <up202107486@edu.fe.up.pt>
2024-02-15 15:29:49 +0000

    Exercício 1

--- tp1/MyDiamond.js
+++ tp1/MyDiamond.js
@@ -18,15 +18,18 @@ export class MyDiamond extends CGFobject {
 			1, 0, 0		//3
 		];
 
-		//Counter-clockwise reference of vertices
+		// Counter-clockwise reference of vertices
 		this.indices = [
 			0, 1, 2,
 			1, 3, 2
 		];
 
-		//The defined indices (and corresponding vertices)
-		//will be read in groups of three to draw triangles
+		// The defined indices (and corresponding vertices)
+		// will be read in groups of three to draw triangles
 		this.primitiveType = this.scene.gl.TRIANGLES;
+
+    // Disable backface culling
+    this.scene.gl.disable(this.scene.gl.CULL_FACE);
 
 		this.initGLBuffers();
 	}
--- tp1/MyInterface.js
+++ tp1/MyInterface.js
@@ -17,12 +17,22 @@ export class MyInterface extends CGFinterface {
         // https://github.com/dataarts/dat.gui/blob/master/API.md
         this.gui = new dat.GUI();
 
-        //Checkbox element in GUI
-        this.gui.add(this.scene, 'displayAxis').name('Display Axis');
+        // Checkbox element in GUI
+        this.gui.add(this.scene, 'displayAxis').name('Axis');
 
-        //Slider element in GUI
+        // Diamond checkbox element in GUI
+        this.gui.add(this.scene, 'displayDiamond').name('Diamond');
+
+        // Triangle checkbox element in GUI
+        this.gui.add(this.scene, 'displayTriangle').name('Triangle');
+
+        // Parallelogram checkbox element in GUI
+        this.gui.add(this.scene, 'displayParallelogram').name('Parallelogram');
+
+        // Slider element in GUI
         this.gui.add(this.scene, 'scaleFactor', 0.1, 5).name('Scale Factor');
 
         return true;
     }
 }
+
