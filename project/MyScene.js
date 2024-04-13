752e0573b5f707e5c4c563df910941e768096b0c
Emanuel Maia <up202107486@edu.fe.up.pt>
2024-04-13 11:00:45 +0100

    Merge

--- project/MyScene.js
+++ project/MyScene.js
@@ -1,18 +1,12 @@
 import { CGFscene, CGFcamera, CGFaxis, CGFappearance, CGFshader, CGFtexture } from "../lib/CGF.js";
-<<<<<<< HEAD
 import { MyPlane } from "./shapes/MyPlane.js";
-import { MySphere } from "./shapes/MySphere.js";
 import { MyFlower } from "./objects/MyFlower.js";
-=======
-import { MyPlane } from "./MyPlane.js";
->>>>>>> 0017bb0385597053aeae9b586c639ebf88eabce8
 
 /**
  * MyScene
  * @constructor
  */
 export class MyScene extends CGFscene {
-<<<<<<< HEAD
 
   constructor() {
     super();
@@ -21,14 +15,6 @@ export class MyScene extends CGFscene {
   init(application) {
     super.init(application);
 
-=======
-  constructor() {
-    super();
-  }
-  init(application) {
-    super.init(application);
-    
->>>>>>> 0017bb0385597053aeae9b586c639ebf88eabce8
     this.initCameras();
     this.initLights();
 
@@ -40,7 +26,6 @@ export class MyScene extends CGFscene {
     this.gl.enable(this.gl.CULL_FACE);
     this.gl.depthFunc(this.gl.LEQUAL);
 
-<<<<<<< HEAD
     // Initialize scene objects
     this.axis = new CGFaxis(this);
     this.plane = new MyPlane(this, 30);
@@ -52,11 +37,6 @@ export class MyScene extends CGFscene {
     // this.stem = new MyStem(this, 20, 20);
     // this.petal = new MyPetal(this);
     this.flower = new MyFlower(this);
-=======
-    //Initialize scene objects
-    this.axis = new CGFaxis(this);
-    this.plane = new MyPlane(this,30);
->>>>>>> 0017bb0385597053aeae9b586c639ebf88eabce8
 
     //Objects connected to MyInterface
     this.displayAxis = true;
@@ -64,7 +44,6 @@ export class MyScene extends CGFscene {
 
     this.enableTextures(true);
 
-<<<<<<< HEAD
     this.texture = new CGFtexture(this, "images/terrain.jpg");
     this.appearance = new CGFappearance(this);
     this.appearance.setTexture(this.texture);
@@ -72,24 +51,13 @@ export class MyScene extends CGFscene {
 
   }
 
-=======
-this.texture = new CGFtexture(this, "images/terrain.jpg");
-this.appearance = new CGFappearance(this);
-this.appearance.setTexture(this.texture);
-this.appearance.setTextureWrap('REPEAT', 'REPEAT');
-
-  }
->>>>>>> 0017bb0385597053aeae9b586c639ebf88eabce8
   initLights() {
     this.lights[0].setPosition(15, 0, 5, 1);
     this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
     this.lights[0].enable();
     this.lights[0].update();
   }
-<<<<<<< HEAD
 
-=======
->>>>>>> 0017bb0385597053aeae9b586c639ebf88eabce8
   initCameras() {
     this.camera = new CGFcamera(
       1.0,
@@ -99,20 +67,14 @@ this.appearance.setTextureWrap('REPEAT', 'REPEAT');
       vec3.fromValues(0, 0, 0)
     );
   }
-<<<<<<< HEAD
 
-=======
->>>>>>> 0017bb0385597053aeae9b586c639ebf88eabce8
   setDefaultAppearance() {
     this.setAmbient(0.2, 0.4, 0.8, 1.0);
     this.setDiffuse(0.2, 0.4, 0.8, 1.0);
     this.setSpecular(0.2, 0.4, 0.8, 1.0);
     this.setShininess(10.0);
   }
-<<<<<<< HEAD
 
-=======
->>>>>>> 0017bb0385597053aeae9b586c639ebf88eabce8
   display() {
     // ---- BEGIN Background, camera and axis setup
     // Clear image and depth buffer everytime we update the scene
@@ -129,14 +91,11 @@ this.appearance.setTextureWrap('REPEAT', 'REPEAT');
 
     // ---- BEGIN Primitive drawing section
 
-<<<<<<< HEAD
     // this.stem.display();
     // this.sphere.display();
     // this.petal.display();
     this.flower.display();
 
-=======
->>>>>>> 0017bb0385597053aeae9b586c639ebf88eabce8
     this.pushMatrix();
     this.appearance.apply();
     this.translate(0,-100,0);
